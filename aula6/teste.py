68fddc95241bc094ebaff07dffc37485be27c188
yasminllz <yasminjhocelinm@gmail.com>
2026-04-10 22:01:48 -0300

    Add files via upload

---
+++ aula6/teste.py
@@ -0,0 +1,24 @@
+# laço de repetição
+
+# for x in "jhocelin":
+#     print(x)
+
+#contagem de 0 a 20 em numeros pares
+# for i in range (0,21,2):
+#     print(i)
+
+#escreva um programa para um vetor de 5 medias dos alunos, fazer a somatoria
+# e imprimir as medias da sala (5 alunos )
+
+# medias = [5.5,10.0,8.0,1.0,6.0]
+# somaMedias = 0 
+# for media in medias:
+#     # print(media)
+#     somaMedias += media
+#     print(f'''Media = {medias} Soma = {somaMedias}''')
+
+# print(somaMedias)
+# print(f"media = {somaMedias/5}")
+
+
+#exercicio - lista fibonacci
